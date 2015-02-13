4897b9dcf06b6c6b576132377bc99690858d5dd8
Jesseract <jlpearce@gmail.com>
2015-02-13 16:23:48 -0500

    Make question field save

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :options
   accepts_nested_attributes_for :options,
+    :allow_destroy => true,
     :reject_if => :all_blank
 
 end
