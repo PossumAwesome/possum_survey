620fa9672d02bdde6cc3bf1807a8cada2483bacf
zachbradshaw <bradshaw.zachary@gmail.com>
2015-02-14 12:18:44 -0500

    removed turbolinks

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -2,7 +2,9 @@ $(function () {
   $('.add-question-button').click(function (event) {
     event.preventDefault();
     alert('i love tom long time');
-    var thisButton = $(this);
-    $('.question').last().clone(true).insertBefore('.actions');
+    // var thisButton = $(this);
+    var buttonContainer = $('.add-button-container');
+    $('.new-question-container').last().clone(true).insertBefore('.actions');
+    buttonContainer.insertBefore('.actions');
   });
 });
