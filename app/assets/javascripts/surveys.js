8f963b29cdbb1f9beb12882fd4ed1f5b5611b4d5
zachbradshaw <bradshaw.zachary@gmail.com>
2015-02-14 10:11:39 -0500

    formatting cloned questions

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -2,7 +2,7 @@ $(function () {
   $('.add-question-button').click(function (event) {
     alert('i love tom long time');
     var thisButton = $(this);
-    $(thisButton).parent().clone(true, true).insertBefore('.actions');
+    $(thisButton).parent().parent().clone(true, true).insertBefore('.actions');
     event.stopPropagation();
     event.preventDefault();
   });
