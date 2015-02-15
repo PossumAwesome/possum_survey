a09fedbb33ceb18e97ac440d49848bfb436ba603
zachbradshaw <bradshaw.zachary@gmail.com>
2015-02-15 17:13:08 -0500

    updated styling

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -1,46 +1,45 @@
 $(function () {
-  $('.add-question-button').click(function (event) {
-    event.preventDefault();
-    // alert('i love tom long time');
-    var buttonContainer = $('.add-button-container');
-    // var slice = $('.hidden-question > p').slice(-5);
-    var clonedItem = $('.hidden-question > div').slice(-1).clone();
-    clonedItem.removeClass('hidden-question-div').addClass('question').insertBefore('.placeholder');
-    changeIndex();
-    $('.hidden-question').insertBefore('.actions');
-    buttonContainer.insertBefore('.actions');
-  });
+ $('.add-question-button').click(function (event) {
+   event.preventDefault();
+   // alert('i love tom long time');
+   var buttonContainer = $('.add-button-container');
+   // var slice = $('.hidden-question > p').slice(-5);
+   var clonedItem = $('.hidden-question > div').slice(-1).clone();
+   clonedItem.removeClass('hidden-question-div').addClass('question').insertBefore('.placeholder');
+   changeIndex();
+   $('.hidden-question').insertBefore('.actions');
+   buttonContainer.insertBefore('.actions');
+ });
 
-  function changeIndex () {
+ function changeIndex () {
 
-    var questionArray = $('.question').toArray();
+   var questionArray = $('.question').toArray();
 
-    for (var i = 0; i < questionArray.length; ++i) {
+   for (var i = 0; i < questionArray.length; ++i) {
 
-      adjustAttrIndex($('.question-field', questionArray[i]), 'name', i) // change its question name
-      adjustAttrIndex($('.question-field', questionArray[i]), 'id', i) // change its question name
-      adjustAttrIndex($('.description-field', questionArray[i]), 'name', i) // change its question name
-      adjustAttrIndex($('.description-field', questionArray[i]), 'id', i) // change its question name
-      adjustAttrIndex($('.required-field', questionArray[i]), 'name', i) // change its question name
-      adjustAttrIndex($('.required-field', questionArray[i]), 'id', i) // change its question name
-      adjustAttrIndex($('.select-field', questionArray[i]), 'name', i) // change its question name
-      adjustAttrIndex($('.select-field', questionArray[i]), 'id', i) // change its question name
+     adjustAttrIndex($('.question-field', questionArray[i]), 'name', i) // change its question name
+     adjustAttrIndex($('.question-field', questionArray[i]), 'id', i) // change its question name
+     adjustAttrIndex($('.description-field', questionArray[i]), 'name', i) // change its question name
+     adjustAttrIndex($('.description-field', questionArray[i]), 'id', i) // change its question name
+     adjustAttrIndex($('.required-field', questionArray[i]), 'name', i) // change its question name
+     adjustAttrIndex($('.required-field', questionArray[i]), 'id', i) // change its question name
+     adjustAttrIndex($('.select-field', questionArray[i]), 'name', i) // change its question name
+     adjustAttrIndex($('.select-field', questionArray[i]), 'id', i) // change its question name
 
-    }
+   }
 
-    function adjustAttrIndex(targetNode, attribute, index) {
-    var currentAttr = targetNode.attr(attribute); // identifies attribute with which we are concerned
-    targetNode.attr(attribute, currentAttr.replace( /\d+/g, index)); // changes it using super sweet regex
-    }
-  };
+   function adjustAttrIndex(targetNode, attribute, index) {
+   var currentAttr = targetNode.attr(attribute); // identifies attribute with which we are concerned
+   targetNode.attr(attribute, currentAttr.replace( /\d+/g, index)); // changes it using super sweet regex
+   }
+ };
 
 
-  // var questionType = $('.survey-question-type')
+ // var questionType = $('.survey-question-type')
 
-  // $('p.survey-question-type:contains("short answer")').append('<input type="text">');
-  // $('p.survey-question-type:contains("true/false")').append('<form><input type="radio" name="truefalse" value="true"> <input type="radio" name="truefalse" value="false"></form>');
-  // $('p.survey-question-type:contains("long answer")').append('<br><textarea>');
-
+ // $('p.survey-question-type:contains("short answer")').append('<input type="text">');
+ // $('p.survey-question-type:contains("true/false")').append('<form><input type="radio" name="truefalse" value="true"> <input type="radio" name="truefalse" value="false"></form>');
+ // $('p.survey-question-type:contains("long answer")').append('<br><textarea>');
 
 
 });
